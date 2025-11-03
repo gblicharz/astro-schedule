841c8bffbac2c9725a06dd58903b565cf9d2dba3
Glenn Blicharz <gblicharz@wttw-wfmt.com>
2025-11-03 16:01:20 -0600

    Converted date page to be dynamic vs. pre-rendered. Added previous/next day navigation. Added URL parameters to image paths to resize (after unsuccessfully getting the unpic package (https://unpic.pics/img/astro/) to resize correctly for both DEV and BUILD environments. Still included the package in case we want to try again.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import { imageService } from "@unpic/astro/service";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
+      enabled: true,
     },
 
     imageService: "cloudflare"
   }),
   vite: {
-   server: {
-     https: {
-       key: './localhost-key.pem',
-       cert: './localhost.pem'
-     }
-   }
- }
+    server: {
+      https: {
+        key: "./localhost-key.pem",
+        cert: "./localhost.pem",
+      },
+    },
+  },
+  /*
+  image: {
+    domains: ["image.pbs.org"],
+    service: imageService({
+      fallbackService: "sharp",
+      placeholder: "lqip",
+      // This is the default
+      layout: "constrained",      
+    }),
+  },
+  */
 });
